6e8ffb48ae3b27ca7d378f44d4792170efa784c0
nyok <nyok@users.noreply.github.com>
2022-05-17 02:26:29 +0300

    Rewrote code for Manifest V3

--- Google Chrome/Extensions/translateInGoogleTranslateJpToEn/background.js
+++ Google Chrome/Extensions/translateInGoogleTranslateJpToEn/background.js
@@ -1,20 +1,13 @@
-// Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
-	var id = chrome.contextMenus.create({
+    chrome.contextMenus.create({
 		"title": "「%s」→ EN", 
 		"contexts":["selection"],
 		"id": "context"
-	});
+    });
 });
-
-// add click event
-chrome.contextMenus.onClicked.addListener(
-	onClickHandler
-);
-
-// The onClicked callback function.
-function onClickHandler(info, tab) {
-	var sText = info.selectionText;
-	var url = "https://translate.google.com/#ja/en/" + encodeURIComponent(sText);	
-	window.open(url, '_blank');
-};
+    
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    chrome.tabs.create({  
+        url: "https://translate.google.com/#ja/en/" + encodeURIComponent(info.selectionText)
+    });
+})
